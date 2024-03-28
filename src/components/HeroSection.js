7957a66bfdedfa2cdb801a9efb0897ed14515dc1
kristofferwaagen <kristofferwagen@gmail.com>
2024-03-28 02:07:50 +0100

    Added animations to loading the hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 import profilePicture from '../assets/profilePicture.png';
 import { TypeAnimation } from 'react-type-animation';
 import '../HeroSection.css'; // Ensure the CSS file name matches
 
 function HeroSection() {
+    const [animationReady, setAnimationReady] = useState(false);
+
+    // Initialize the fade and scale animation
+    const fadeAndScale = useSpring({
+        to: { opacity: 1, transform: 'scale(1)' },
+        from: { opacity: 0, transform: 'scale(2)' },
+        delay: 500, // Adjust delay as necessary
+        onRest: () => setAnimationReady(true), // When animation completes, set animationReady
+    });
+
     const handleClick = () => {
-        // Functionality for when the image is clicked
         console.log('Image clicked, load new content here.');
     };
 
     return (
-        <div className="profile-container">
-            <div className="text-content">
-                <TypeAnimation
-                    sequence={[
-                        'Hello, my name is Kristoffer',
-                        1000,
-                        "I'm a computer science student",
-                        1000,
-                        'Find out more by clicking my picture',
-                        1000,
-                    ]}
-                    wrapper="span"
-                    speed={60}
-                    style={{ fontSize: '2em', display: 'inline-block' }}
-                    repeat={Infinity}
-                />
-            </div>
+        <animated.div style={fadeAndScale} className="profile-container">
+            {animationReady && (
+                <div className="text-content">
+                    <TypeAnimation
+                        sequence={[
+                            'Hello, my name is Kristoffer',
+                            1000,
+                            "I'm a computer science student",
+                            1000,
+                            'Find out more by clicking my picture',
+                            1000,
+                        ]}
+                        wrapper="span"
+                        speed={60}
+                        style={{ fontSize: '2em', display: 'inline-block' }}
+                        repeat={Infinity}
+                    />
+                </div>
+            )}
             <img src={profilePicture} alt="Profile" onClick={handleClick} />
-        </div>
+        </animated.div>
     );
 }
 
